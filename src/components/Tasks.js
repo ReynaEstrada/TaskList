782523ff354d012f8566b5da04136792b1939d08
JosMIgue <jojo_miguel20@hotmail.com>
2022-10-04 10:14:24 -0500

    Add autoanimate library

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+//import PropTypes from 'prop-types';
 import ListItem from './ListItem';
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 
-class Tasks extends Component {
-    render(){
-        return  this.props.tasks.map(task => 
-            <ListItem task={task} key={task.id}  deleteTask={this.props.deleteTask} checkDone={this.props.checkDone}></ListItem>
-        );
-    }
-}
-Tasks.propTypes = {
-    tasks: PropTypes.array.isRequired
+const Tasks = function(props){
+    const [list] = useAutoAnimate(/* optional config */)
+    return(
+        <div ref={list}>
+            {
+                props.tasks.map(
+                    task => <ListItem task={task} key={task.id}  deleteTask={props.deleteTask} checkDone={props.checkDone}></ListItem>
+                )
+            }
+        </div>
+    )
 }
 export default Tasks;
